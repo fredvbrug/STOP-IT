bab91da2a9831c476d76d0eecd76fc956ca3957b
Frederick <frederick.verbruggen@gmail.com>
2019-02-21 14:33:29 +0100

    add readme for analysis program

---
+++ jsPsych_version/README-ANALYZE-IT-JS.md
@@ -0,0 +1,45 @@
+# ANALYZE-IT-JS: Analyze the data of STOP-IT jsPsych using app.R
+
+*app.R*  in the jsPsych folder can be used to analyze the data produced by the jsPsych version of STOP-IT, and is available under a GNU license. The software uses Shiny, *"an open source R package that provides an elegant and powerful web framework for building web applications using R."* For general information about Shiny, see https://www.rstudio.com/products/shiny/
+
+## Installation
+
+For data-protection reasons, the analysis program has to run locally. The installation requires only two simple steps:
+
+- Step 1: Install R (https://www.r-project.org)
+- Step 2: Open the R console and type "install.packages('shiny’)”. Restart R.
+
+Note that Steps 1 and 2 can be skipped if R and the shiny package are already installed.
+
+## How to use the app?
+
+- Step 1: Open R, and type in the console: **shiny::runGitHub('STOP-IT', 'fredvbrug', launch.browser = TRUE, subdir = 'jsPsych_version')**. This will open a local version of the app in the default web browser.
+- Step 2: Provide the required input.
+  - Step 2a: Indicate whether the program should include all trials or only trials in fullscreen mode. By default, the experiment is run in fullscreen mode, but this can be changed by the experimenter or participant (during the experiment). Note that trials on which the participant did not interact with the experiment window (see the README-STOP-IT-JS file) are automatically excluded.
+  - Step 2b: Select the data files that should be processed. Multiple data files can be selected at once.
+  - Step 2c: Click on the 'process data' button to process the selected data files.
+
+Our analysis software complies with the recommendations described in "Capturing the ability to inhibit actions and impulsive behaviors: A consensus guide to the stop-signal task" (Verbruggen et al., 2019), and stop-signal reaction time (SSRT) is estimated using the integration method (with replacement of go omissions). We refer researchers to this guide for detailed information about the estimation procedure. Note that the program (*app.R*) uses the Type-6 quantile method to determine the nth RT (for more information, see https://stat.ethz.ch/R-manual/R-devel/library/stats/html/quantile.html).
+
+Once the data are processed, users will immediately get an overview. There are three output panels:
+
+- The figure panels plots the SSRT distribution
+- The table in the 'Summary' panel provides a general summary of the main dependent variables.
+- The table in the 'Individual data' panel shows the main dependent variables for each individual.
+
+In both table panels, there are 11 dependent variables:
+- presp = p(respond|signal) = probability of responding on a stop trial. P(respond|signal) is also used to determine SSRT.
+- ssd = stop-signal delay = the average delay between the presentation of the go stimulus and the stop-signal (in ms)
+- ssrt = stop-signal reaction time = how long does it (on average) take to stop a response (in ms)?
+- usRT = RT on unsuccessful stop trials (in ms)
+- goRT_all = reaction time (RT) on go trials with a response (in ms). This includes choice errors.
+- goRT_correct = reaction time (RT) on go trials with a correct response (in ms). Choice errors are excluded.
+- goRT_sd = intra-subject variability in response latencies
+- go_omission = proportion of go trials without a response
+- go_error = proportion of incorrect responses on go trials (e.g. the go stimulus required a left response but a right response was executed)
+- Nstop = number of stop trials included in the analyses
+- NGo = number of go trials included in the analyses.
+
+Note: to test the assumption of the independent race model (see Verbruggen et al., 2019), users should compare usRT with goRT_all (i.e. usRT < goRT_all).
+
+The data shown in the tables can be downloaded via the corresponding buttons. Finally, there is a third download button, which appears in the 'Optional' panel. Users can use this button to transform the STOP-IT jsPsych output to a format that is appropriate for parametric testing. See http://dora.erbe-matzke.com/software.html for further information.
--- jsPsych_version/app.R
+++ jsPsych_version/app.R
@@ -3,58 +3,58 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("SSRT estimator"),
   fluidRow(
-    
+
     column(3,
            h1("Input"),
-           
+
            # Input: specify if we should include fullscreen trials only
            h4("Step 1: Trial inclusion criteria"),
            radioButtons("scr", "Fullscreen trials only?",
                         choices = c(Yes = "yes",
                                     No = "no"),
                         selected = "yes"),
-           
+
            # Include clarifying text ----
            helpText("By default, the experiment is run in fullscreen mode.
                     This can be changed by the experimenter or participant (during the experiment).
-                    You can choose to include only fullscreen trials. 
+                    You can choose to include only fullscreen trials.
                     Trials on which the focus was not on the screen are automatically excluded."),
-           
+
            br(),
            h4("Step 2: Upload the data"),
            fileInput("csvs", label=NULL, multiple = TRUE),
-           
+
            # Include clarifying text ----
-           helpText("Use the browse function to upload all the data files. 
-                    You can upload all requested files at once 
+           helpText("Use the browse function to upload all the data files.
+                    You can upload all requested files at once
                     (when running the Shiny app via a browser)."),
-           
-           
+
+
            br(),
            h4("Step 3: Process the data"),
            actionButton("process", "Click here to process data"),
-           
+
            conditionalPanel("input.process",
                             br(),
                             br(),
                             h4("Optional"),
                             downloadButton('downloadBeest', "Data for parametric tests"), # download button
-                            
-                            helpText("You can use this option to put the raw data 
+
+                            helpText("You can use this option to put the raw data
                                      in the appropriate format for further parametric testing.")
            )
     ),
-    
+
     column(7,
            conditionalPanel("input.process",
                             h1("Output"),
                             plotOutput(outputId = "main_plot", height = "300px"),
                             h4("Summary"),
-                            downloadButton('downloadSummary', "Download table"), # download button 
+                            downloadButton('downloadSummary', "Download table"), # download button
                             tableOutput('summary'),
                             br(),
                             h4("Individual data"),
-                            downloadButton('downloadIndividuals', "Download table"), # download button 
+                            downloadButton('downloadIndividuals', "Download table"), # download button
                             tableOutput('table'))
     )
   )
@@ -67,57 +67,57 @@ server <- function(input, output) {
     do.call("rbind", (lapply(input$csvs$datapath, function(i){
       read.csv(i, header=TRUE, na.strings=c("null"), colClasses=c("rt"="integer"))})))
   }))
-  
-  
+
+
   processed<-reactive({
     nsubjects <- unique(data()$participantID)
     for (i in nsubjects){
       # select the relevant experimental data for the participant
       dataset<-reactive({subset(data(), data()$participantID == i & data()$block_i > 0)})
-      
-      
+
+
       # exclude trials when particpant is not focusing
       if (input$scr == 'yes'){
-        dataset.focus <-reactive({subset(dataset(), dataset()$focus == 'focus' 
+        dataset.focus <-reactive({subset(dataset(), dataset()$focus == 'focus'
                                          && dataset()$Fullscreen == 'yes')})}
       else{
         dataset.focus <-reactive({subset(dataset(), dataset()$focus == 'focus')})
       }
-      
+
       # stop-signal trials
       stopsignal<-reactive({subset(dataset.focus(), dataset.focus()$signal == 'yes')})
       Nstop <- nrow(stopsignal())
-      
+
       tmp <- ifelse(stopsignal()$response == "undefined", 0, 1)
       presp <- mean(tmp)
       ssd <- round(mean(stopsignal()$SSD))
-      
+
       stopsignal.resp.trials <- reactive({subset(stopsignal(), stopsignal()$response != "undefined")})
       usRT <- round(mean(stopsignal.resp.trials()$rt))
-      
+
       # go trials
       go<-reactive({subset(dataset.focus(), dataset.focus()$signal == 'no')})
       Ngo <- nrow(go())
-      
+
       go.resp.trials <- reactive({subset(go(), go()$response != "undefined")})
       goRT_all <- round(mean(go.resp.trials()$rt))
       goRT_sd <- round(sd(go.resp.trials()$rt))
-      
+
       goRT.max <- max(go.resp.trials()$rt)
       goRT.adj <- ifelse(go()$response == "undefined", goRT.max, go()$rt)
       nth <- as.vector(round(quantile(goRT.adj, probs = presp, type = 6)))
-      
+
       go.correct.trials <- reactive({subset(go(), go()$correct == "true")})
       goRT_correct <- round(mean(go.correct.trials()$rt))
-      
+
       go_omission <- 1 - (nrow(go.resp.trials())/Ngo)
       go_error <-  1 - (nrow(go.correct.trials())/Ngo)
-      
+
       # calculate SSRT
       ssrt <- nth - ssd
-      
+
       # combine everything
-      dp.subject <- data.frame(presp, ssd, nth, ssrt, usRT, goRT_all, goRT_correct, goRT_sd,
+      dp.subject <- data.frame(presp, ssd, ssrt, usRT, goRT_all, goRT_correct, goRT_sd,
                                go_omission, go_error, Nstop, Ngo, subject=i)
       if (exists("dp.all")==FALSE){
         dp.all <- dp.subject
@@ -127,12 +127,12 @@ server <- function(input, output) {
     }
     dp.all
   })
-  
+
   # table with the processed data for all subjects
   output$table <- renderTable({
     processed()
   })
-  
+
   # generate a summary of the dataset
   datasum <-reactive({
     dataset <- reactive({data.frame(processed(), row.names = NULL)})
@@ -162,17 +162,17 @@ server <- function(input, output) {
     s.table <- cbind (variable = dvs, s.table)
     s.table
   })
-  
+
   output$summary <- renderTable({
     datasum()
   })
-  
+
   # figure with the distribution of SSRTs
   output$main_plot <- renderPlot({
     tmp <- reactive({data.frame(processed(), row.names = NULL)})
     hist(tmp()$ssrt, xlab="Stop-signal reaction times (SSRT)", main='SSRT distribution')
   })
-  
+
   # ---- files for downloads
   # individual data
   #downloadHandler() takes two arguments: filename and content.
@@ -180,22 +180,22 @@ server <- function(input, output) {
     filename = "stop_individual_data.csv",
     content = function(file) {write.table(processed(), file, sep = ",", row.names = FALSE)}
   )
-  
+
   # summary data
   #downloadHandler() takes two arguments: filename and content.
   output$downloadSummary <- downloadHandler(
     filename = "stop_summary_data.csv",
     content = function(file) {write.table(datasum(), file, sep = ",", row.names = FALSE)}
   )
-  
+
   # also put the data in BEESTs-appropriate format
   dataBeest <-reactive({
   if (input$scr == 'yes'){
     data.focus <-reactive({subset(data(), data()$focus == 'focus' && data()$fullscreen == 'yes')})}
-  else{ 
+  else{
     data.focus <-reactive({subset(data(), data()$focus == 'focus')})
   }
-  
+
   subj_idx <- data()$participantID
   ss_presented	<- ifelse(data()$signal == 'yes', 1, 0)
   tmp	<- ifelse(data()$correct == 'true', 1, 0)
@@ -204,22 +204,21 @@ server <- function(input, output) {
   ssd	<- ifelse(data()$signal == 'no', -999, tmp)
   tmp <- data()$rt
   rt	<- ifelse(data()$rt == 'NA', -999, tmp)
-  
+
   beest.df <- data.frame(subj_idx, ss_presented, inhibited, ssd, rt)
   beest.df
   })
-  
+
   output$Beest <- renderTable({
     dataBeest()
   })
-  
+
   output$downloadBeest <- downloadHandler(
     filename = "stop_beest_data.csv",
     content = function(file) {write.table(dataBeest(), file, sep = ",", row.names = FALSE)}
   )
-  
+
 }
 
 
 shinyApp(ui = ui, server = server)
-
