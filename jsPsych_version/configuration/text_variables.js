64b1f1dd5d88b2b24674bd37c8e1b49c73212dac
Frederick Verbruggen <frederick.verbruggen@gmail.com>
2019-03-28 09:47:11 +0100

    Update text_variables.js
    fix minor typo in the instructions

--- jsPsych_version/configuration/text_variables.js
+++ jsPsych_version/configuration/text_variables.js
@@ -12,7 +12,7 @@ var page1 = [
 // Do not forget to adjust the number of blocks
 page2 = [
   '<p> Nevertheless, it is really important that you do not wait for the stop signal to occur and that you respond as quickly and as accurately as possible to the arrows. </p>'+
-  '<p> After all, if you start waiting for stop signals, than the program will delay their presentation. This will result in long reaction times. </p>'+
+  '<p> After all, if you start waiting for stop signals, then the program will delay their presentation. This will result in long reaction times. </p>'+
   '<p> We will start with a short practice block in which you will receive immediate feedback. You will no longer receive immediate feedback in the experimental phase. </p>'+
   '<p> However, at the end of each experimental block, there will be a 15 second break. During this break, we will show you some information about your mean performance in the previous block.</p>' +
   '<p> The experiment consists of 1 practice block and 4 experimental blocks</p>'
